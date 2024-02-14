9cb5250ee0f86ec1c35aea3a89f6eed985117315
andredeve <andreluizzz299@gmail.com>
2024-02-14 13:40:35 -0400

    Feat: Corrigir a home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,12 @@
 import './home.css';
 import Logo from '../../assets/Logo.png';
 
-
 export default function Home(){
-
     return(
       <div className='home-container'>
 
-        <div className='login-area'>
-            <img src={Logo}/>
-        </div>
-        
-        <span>A sua loja online de suplementos</span>
+          <img src={Logo}/>
+          <h1>A sua loja online de suplementos</h1> 
 
       </div>
     )
