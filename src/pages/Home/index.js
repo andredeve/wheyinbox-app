b0ea8145286da0a2216f57be34785e601286d83e
andredeve <andreluizzz299@gmail.com>
2024-02-14 13:32:04 -0400

    Feat: Melhorando a home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,8 @@
-import { useState } from 'react';
 import './home.css';
-
 import Logo from '../../assets/Logo.png';
 
-import {Link} from 'react-router-dom';
 
 export default function Home(){
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
     return(
       <div className='home-container'>
@@ -16,34 +10,8 @@ export default function Home(){
         <div className='login-area'>
             <img src={Logo}/>
         </div>
-      
-        {/*   <h1>WheyInBox</h1> */}
-        {/* <span>A sua loja online de suplementos</span> */}
-        <form className='form'>
-          <input
-            type='text'
-            placeholder='E-mail'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-         />
-
-        <input
-            autoComplete={false}
-            type='password'
-            placeholder='Senha' 
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-         />
         
-        <button type='submit'>Entrar</button>
-        </form>
-
-        <Link
-         className='button-link'
-         to="/register"
-        >
-          Não possuí uma conta? Cadastre-se
-        </Link>
+        <span>A sua loja online de suplementos</span>
 
       </div>
     )
