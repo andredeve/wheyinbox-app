7b6e4db5f260ebf6483640a72a1f3f1e5294f463
andredeve <andreluizzz299@gmail.com>
2024-02-14 13:50:22 -0400

    Fix: Resolvendo erro na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,15 @@ import './home.css';
 import Logo from '../../assets/Logo.png';
 
 export default function Home(){
+    
     return(
       <div className='home-container'>
 
-          <img src={Logo}/>
-          <h1>A sua loja online de suplementos</h1> 
+        <div className='login-area'>
+            <img src={Logo}/>
+        </div>
+    
+    
 
       </div>
     )
