1c137946f40451f18f6741207991d4f7ef19384a
andredeve <andreluizzz299@gmail.com>
2024-02-16 11:07:27 -0400

    Fix: bug na home

--- src/components/Marcas/index.js
+++ src/components/Marcas/index.js
@@ -7,11 +7,10 @@ import logo5 from '../../assets/logo5.png';
 import logo6 from '../../assets/logo6.png';
 import './marcas.css'; // Importe o CSS para estilização
 
-//teste
 function Marcas() {
   return (
     <div className="brand-grid-container"> 
-        <h3 className="brand-grid-title">As Melhores Marcas</h3>
+        <h3 className="brand-grid-title">As melhores marcas</h3>
         <div className="brand-grid">
             <img src={logo1} alt="Logo 1" className="brand-logo" />
             <img src={logo2} alt="Logo 2" className="brand-logo" />
