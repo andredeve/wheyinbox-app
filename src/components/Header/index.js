7e403986f10a0e29e433a9174853446523b452e3
andredeve <andreluizzz299@gmail.com>
2024-02-15 22:17:56 -0400

    Feat: Criação de Menu e BrandGrid

---
+++ src/components/Header/index.js
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import './header.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faUser, faQrcode } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { AuthContext } from '../../contexts/auth';
+import Logo from '../../assets/Logo.png';
+
+export default function Header(){
+
+    const {logout} = useContext(AuthContext);
+
+    async function handlerLogout(){
+      await logout();
+    }
+
+    return(
+        <header>
+            <div className="header-content">
+                
+                <div className="logo-container">
+                    <img src={Logo} alt="Logo" className="logo" />
+                </div>
+
+                <button className="hamburger-button">
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+                <div className="search-container">
+                    <FontAwesomeIcon icon={faQrcode} className="qrcode-icon" />
+                    <input type="text" placeholder="Ache seu suplemento" className="search-bar" />
+                </div>
+                <button className="cart-button">
+                    <FontAwesomeIcon icon={faShoppingCart} /> Carrinho
+                </button>
+                <button className="logout-button">
+                    <FontAwesomeIcon icon={faUser} /> Sair
+                </button>
+            </div>
+        </header>
+    )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,14 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
-
+import Header from "../../components/Header";
+import Marcas from "../../components/Marcas";
 
 export default function Home(){
 
-  const {logout} = useContext(AuthContext);
-
-  async function handlerLogout(){
-    await logout();
-  }
   return(
     <div>
-      <h1>Pagina Dashboard</h1>
-      <button onClick={handlerLogout}>Sair</button>
+      <Header/>
+      <Marcas/>
     </div>
   )
 }
